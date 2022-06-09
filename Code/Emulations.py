ae9310b1a2fddef465e4891a56de0c675aa39def
Polina <polina.korogodska@gmail.com>
2022-06-09 18:27:33 +0300

    Emulate func with parameteres passed by registers

--- Code/Emulations.py
+++ Code/Emulations.py
@@ -1,6 +1,7 @@
 import r2pipe
 import Code.FunctionClass
 
+
 def emulate_function(func, binary):
     r = r2pipe.open(binary)
     addr = "0x"+hex(func.offset)[2:].zfill(8)
@@ -24,12 +25,32 @@ def emulate_function(func, binary):
     #print(aesu)
     #  emulate from the start of the func until specified address
     print(r.cmd(aesu))
-    print("HI")
+    #print("HI")
     #print(r.cmd())
     print(r.cmd("ar"))
-    print("Expelliarmus")
+    #print("Expelliarmus")
     print(r.cmd("ad@r:SP"))
-    print("Lumos")
+    #print("Lumos")
     print(r.cmd("ad@r:SP"))
-    print("Bye")
+    #print("Bye")
 
+
+def emulate_function_with_params(func, binary):
+    r = r2pipe.open(binary)
+    addr = "0x"+hex(func.offset)[2:].zfill(8)
+    s = 's '+addr
+    r.cmd(s)
+    print(r.cmd("pd"))
+    #  initialize stack machine for emulation
+    r.cmd("aeim")
+    for reg in func.params:
+        r.cmd(f'aer {reg} = {func.params[reg]}')
+    #  working commands: 'ar,' - table view; 'ar' - register and value
+    print(r.cmd("ar"))
+    print(r.cmd("ad@r:SP"))
+    aesu = 'aesu 0x' + hex(func.offset + func.length-1)[2:].zfill(8)
+    #  emulate from the start of the func until specified address
+    print(r.cmd(aesu))
+    print(r.cmd("ar"))
+    print(r.cmd("ad@r:SP"))
+    print(r.cmd("ad@r:SP"))
